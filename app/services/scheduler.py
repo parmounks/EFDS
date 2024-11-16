e6893417a79ad89e4b40a756ac7a2599b9e51dd9
inangie <186221676+inangie@users.noreply.github.com>
2024-11-15 22:41:17 -0500

    Revert "Backend draft"
    This reverts commit ec9b646dbdaa7fce384139dc487f702f782d809e.

--- app/services/scheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from app.services.fetch_image import fetch_latest_image
-
-def init_scheduler():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(fetch_latest_image, 'interval', hours=0.5)
-    scheduler.start()
